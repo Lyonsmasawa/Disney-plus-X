1330a097da86bcf93ac98c184222c629068db614
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-23 14:06:38 -0700

    styled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -58,6 +58,23 @@ const Logo = styled.img`
     margin-bottom: 12px;
 `;
 
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
+`
+
 const Login = () => {
     return(
         <Container>
