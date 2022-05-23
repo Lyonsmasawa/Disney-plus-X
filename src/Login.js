00c7e28eae4077d2932136475481bec38030187f
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-23 13:04:38 -0700

    styled component

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Login = () => {
-    return (
-        <div className="login">
-            <h1>Login</h1>
-        </div>
-    )
+    <Container>
+        <Content>Center</Content>
+    </Container>
 }
-
+    const Container = styled
 export default Login
