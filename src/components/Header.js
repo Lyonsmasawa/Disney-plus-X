896e07d4f32fa72777cb2e0aa1f59ea40ec2e793
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-23 14:58:23 -0700

    build header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import styled from "styled-components"
 
 const Nav = styled.nav`
-    
+    position: fixed;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 70px;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 0 36px;
+    leter-spacing: 16px;
+    z-index: 1001;
 `
  
  const Header = () => {
    return (
-     <Nav>Header</Nav>
+     <Nav>
+        <Logo>
+            <img src="https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original" alt="Disney+" />
+        </Logo>
+    </Nav>
    )
  }
  
