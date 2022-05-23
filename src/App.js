776a4a6ab31dfb929ddd5c7d5206f7971e923eff
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-23 01:04:46 -0700

    configure routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Router } from 'react-router-dom';
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
+        <Routes>
+          <Route />
+        </Routes>
       </Router>
     </div>
   );
