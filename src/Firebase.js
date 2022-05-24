76c8cf19d1f5dff5ed142ea8788011b0c915e854
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-24 02:26:36 -0700

    set up firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -19,6 +20,6 @@ const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
 
-export {auth, provider, storage}
+export {auth, provider, storage};
 export default db;
 
