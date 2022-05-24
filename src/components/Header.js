2ed122750113403cc32e7469b0c352d8f6639209
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-24 01:18:50 -0700

    build header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,45 +43,43 @@ const NavMenu = styled.div`
     padding: 0px;
     margin-right: auto;    
     margin-left: 25px;
+    }
 
-    span {
-        color: rgb(249, 249, 249);
-        font-size: 14px;
-        font-weight: 600;
-        letter-spacing: 1.8px;
-        padding: 2px 0px;
-        white-space: nowrap;
-        position: relative; 
-
-        
-        &:before {
-            content: "";
-            background-color: rgb(249, 249, 249);
-            bottom: -3px;
-            height: 2px;
-            left: 0px;
-            opacity: 0;
-            position: absolute;
-            right: 0px;
-            transform-origin: left center;
-            transform: scaleX(0);
-            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-            visibility: hidden;
-            width: auto;
-        }
+    @media(max-width: 768px){
+        display: none;
+    }
+`
+const Span = styled.span`
+    color: rgb(249, 249, 249);
+    font-size: 14px;
+    font-weight: 600;
+    letter-spacing: 1.8px;
+    padding: 2px 0px;
+    white-space: nowrap;
+    position: relative; 
+    
+    &:before {
+        content: "";
+        background-color: rgb(249, 249, 249);
+        bottom: -3px;
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
     }
 
     &:hover {
-        span:before {
+        :before {
             transform: scaleX(1);
             visibility: visible;
             opacity: 1 !important;
         }
-    }
-
-
-    @media(max-width: 768px){
-        display: none;
     }
 `
 
@@ -140,12 +138,12 @@ const Rotate = styled.div`
             <img src="https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original" alt="Disney+" />
         </Logo>
         <NavMenu>
-            <Link to="/home" style={MenuLinks}><HomeOutlined style={MenuIcon} /><span>HOME</span></Link>
-            <Link to="/home" style={MenuLinks}><Search style={MenuIcon} /><span>SEARCH</span></Link>
-            <Link to="/home" style={MenuLinks}><Add style={MenuIcon} /><span>WATCHLIST</span></Link>
-            <Link to="/home" style={MenuLinks}><Star style={MenuIcon} /><span>ORIGINALS</span></Link>
-            <Link to="/home" style={MenuLinks}><Hd style={MenuIcon} /><span>MOVIES</span></Link>
-            <Link to="/home" style={MenuLinks}><Tv style={MenuIcon} /><span>SERIES</span></Link>
+            <Link to="/home" style={MenuLinks}><HomeOutlined style={MenuIcon} /><Span>HOME</Span></Link>
+            <Link to="/home" style={MenuLinks}><Search style={MenuIcon} /><Span>SEARCH</Span></Link>
+            <Link to="/home" style={MenuLinks}><Add style={MenuIcon} /><Span>WATCHLIST</Span></Link>
+            <Link to="/home" style={MenuLinks}><Star style={MenuIcon} /><Span>ORIGINALS</Span></Link>
+            <Link to="/home" style={MenuLinks}><Hd style={MenuIcon} /><Span>MOVIES</Span></Link>
+            <Link to="/home" style={MenuLinks}><Tv style={MenuIcon} /><Span>SERIES</Span></Link>
         </NavMenu>
         <Rotate>.</Rotate>
         <Login>Log in</Login>
