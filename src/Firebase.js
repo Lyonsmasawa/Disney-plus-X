151a8b6721bde4c0327dadf14226e451a3379988
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-24 01:53:14 -0700

    set up firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,4 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
