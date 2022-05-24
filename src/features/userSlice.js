5c7f9c65260254e92210d4e9c48d66b8923277ce
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-24 04:24:54 -0700

    create user slice

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -24,3 +24,8 @@ const userSlice = createSlice({
     },
 
 });
+
+export const {setUserLoginDetails, setSignOutState} =  userSlice.actions;
+export const selectUserName = state => state.user.name;
+export const selectUserEmail = state => state.user.email;
+export const selectUserPhoto = state => state.user.photo;
