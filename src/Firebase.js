bdd90f811fe6d7ad20989275f0506227522d0cb8
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-24 01:12:50 -0700

    set up firebase\

--- src/Firebase.js
+++ src/Firebase.js
@@ -14,4 +14,11 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = 
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export {auth, provider, storage}
+export default db;
+
