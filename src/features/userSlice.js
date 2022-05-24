e6425ca6dd967c320756e28c498dfbc9c1d6ecf0
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-24 04:25:09 -0700

    export data

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -26,6 +26,6 @@ const userSlice = createSlice({
 });
 
 export const {setUserLoginDetails, setSignOutState} =  userSlice.actions;
-export const selectUserName = state => state.user.name;
-export const selectUserEmail = state => state.user.email;
-export const selectUserPhoto = state => state.user.photo;
+export const selectUserName = state => state.user.name; 
+export const selectUserEmail = state => state.user.email; 
+export const selectUserPhoto = state => state.user.photo; 
