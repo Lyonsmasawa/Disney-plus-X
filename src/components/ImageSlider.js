e18869ac9abe20bc174b7db62a66323812dbf6ad
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-25 09:28:06 -0700

    use slick

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components'
 
 export const ImageSlider = () => {
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    }
   return (
-    <div>ImageSlider</div>
+    <>mage</>
   )
 }
