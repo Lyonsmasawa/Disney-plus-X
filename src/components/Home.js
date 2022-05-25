65b4e96e8abf81ed7c2339e088a5071e491e772a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-25 03:22:55 -0700

    build home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;  
+    display: block;
+    top: 72px;
+    padding: 0 calc(1vw + 2px);
+
+    &:after {
+        background: url("/images/home-background.png") center center / cover no-repeat fixed;
+        content: "";
+        inset: 0;
+        // top: 0;
+        // left: 0;
+        // right: 0;
+        // bottom: 0;
+        position: absolute;
+        z-index: -1;
+        opacity: 1;
+    }
+`
 
 export const Home = (props) => {
+
   return (
-    <div>Home</div>
+    <Container>
+            <h2>Home</h2>
+            <h2>Home</h2>
+    </Container>
   )
 }
 
