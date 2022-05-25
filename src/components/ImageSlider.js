c4d9efaad380a49d4b4496f38b6fb1325ba478d0
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-25 10:04:32 -0700

    style carousel

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -18,6 +18,14 @@ const Carousel = styled(Slider)`
 
     & > button {
         opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+
+        &:hover{
+            opacity: 1;
+            transition: opacity 0.2s ease 0s;
+        }
     }
 `
 
