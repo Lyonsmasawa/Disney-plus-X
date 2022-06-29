2048785f1413f3453816842d48f90f057479ef5e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-29 03:57:51 -0700

    build detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -93,7 +93,42 @@ const Detail = () => {
 
     const Trailer = styled(Player)`
         background: rgba(0, 0, 0, 0.3);
+        border: 1px solid rgb(249, 249, 249);
         color: rgb(249, 249, 249);
+
+        &:hover {
+            background: rgb(198, 198, 198);
+        }
+    `
+
+    const AddList = styled.div`
+        margin-right: 16px;
+        height: 44px;
+        width: 44px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgba(0, 0, 0, 0.6);
+        border-radius: 50%;
+        border: 2px solid white;
+        cursor: pointer;
+
+        span {
+            background-color: rgb(249, 249, 249);
+            display: inline-block;
+            
+            &:first-child {
+                height: 2px;
+                transform: translate(1px, 0px) rotate(0deg);
+                width: 16px;
+            }
+
+            &:nth-child(2) {
+                height: 16px;
+                transform: translateX(-8px) rotate(0deg);
+                width: 2px;
+            }
+        }
     `
 
   return (
@@ -119,6 +154,10 @@ const Detail = () => {
                     <img src="/images/play-icon-white.png" alt="" />
                     <span>Trailer</span>
                 </Trailer>
+                <AddList>
+                    <span />
+                    <span />
+                </AddList>
             </Controls>
         </ContentMeta>
 
