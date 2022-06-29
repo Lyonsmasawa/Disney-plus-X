ceba6c05cd396022e8616b9ff5e1639099450855
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-28 23:51:37 -0700

    Viewers section videos

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -44,18 +44,33 @@ const Viewers = () => {
     <Container>
         <Wrap>
             <img src="/images/viewers-disney.png" alt="" srcset="" />
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src='/videos/1564674844-disney.mp4' type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-pixar.png" alt="" srcset="" />
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src='/videos/1564676714-pixar.mp4' type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-marvel.png" alt="" srcset="" />
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src='/videos/1564676115-marvel.mp4' type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-starwars.png" alt="" srcset="" />
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src='/videos/1608229455-star-wars.mp4' type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-national.png" alt="" srcset="" />
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4'/>
+            </video>
         </Wrap>
     </Container>
   )
