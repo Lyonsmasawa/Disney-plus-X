993d2ecd619f9c99fd9358d3ad3630777ad16cbc
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-29 00:52:06 -0700

    movie slice

---
+++ src/features/movie/movieSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    recommended: null,
+    newDisney: null,
+    original: null,
+    trending: null,
+};
+
+const movieSlice = createSlice({
+    name: 'movie',
+    initialState,
+    reducer: {
+        setMovies: (state, action) => {
+            state.recommended = action.payload.recommended;
+            state.newDisney = action.payload.newDisney;
+            state.original = action.payload.original;
+            state.trending = action.payload.trending;
+        },
+    },
+});
+
+
+export const {setMovies} = movieSlice.actions
+
+export const selectRecommended = state => state.movie.recommended;
+export const selectNewDisney = state => state.movie.newDisney;
+export const selectOriginal = state => state.movie.original;
+export const selectTrending = state => state.movie.trending;
+
+export default movieSlice.reducer;
