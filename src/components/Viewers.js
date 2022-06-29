e67681fd19534a4b211e25c45bb9d642976b3b66
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-28 23:45:31 -0700

    Viewers section

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -16,13 +16,47 @@ const Viewers = () => {
         }
     `
 
+    const Wrap = styled.div`
+        padding-top: 56.25%;
+        border-radius: 10px;
+        box-shadow: rgb( 0 0 0 / 69%) 0px 26px 30px -10px, rgb( 0 0 0 / 73%) 0px 16px 10px -10px;
+        cursor: pointer;
+        overflow: hidden;
+        position: relative;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        border: 2px solid rgba(249, 249, 249, 0.1);
+
+        img {
+            inset: 0px;
+            display: block;
+            height: 100%;
+            object-fit: cover;
+            opacity: 1;
+            position: absolute;
+            transition: all 500ms ease-in-out;
+            width: 100%;
+            z-index: 1;
+            top: 0;
+        }
+    `
+
   return (
     <Container>
-        <div className="">1</div>
-        <div className="">2</div>
-        <div className="">3</div>
-        <div className="">4</div>
-        <div className="">5</div>
+        <Wrap>
+            <img src="/images/viewers-disney.png" alt="" srcset="" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-pixar.png" alt="" srcset="" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-marvel.png" alt="" srcset="" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-starwars.png" alt="" srcset="" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-national.png" alt="" srcset="" />
+        </Wrap>
     </Container>
   )
 }
